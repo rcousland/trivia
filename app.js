49839ac69c04537d113b29e35f3f40ab950b2baf
Rory <rcousland@gmail.com>
2018-01-14 14:41:04 +0000

    code cleanup

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const config = require('./config/express.js');
-const host = config.host;
-const port = config.port;
+const host = require('./config/express.js').host;
+const port = require('./config/express.js').port;
 
 // Load routes
 app.use(express.static('public'));
 app.use('/user', require('./routes/user.js'));
-//require('./routes/user.js')(app);
 
 // Start express
 app.listen(port, host, () => {
