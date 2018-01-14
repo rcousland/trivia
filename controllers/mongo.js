041251356cbe1b6f5efca4e1b63784dd7ecff21f
Rory <rcousland@gmail.com>
2018-01-14 15:48:48 +0000

    add mongojs connector

--- controllers/mongo.js
+++ controllers/mongo.js
@@ -1,11 +1,17 @@
-module.exports = (db) => {
-    db.on('error', (err) => {
-        console.log('database error', err)
-    })
-    db.on('connect', () => {
-        console.log('database connected')
-    })
-    db.on('close', () => {
-        console.log('database disconnected')
-    })
-}
+const mongojs = require('mongojs');
+const mongoUrl = require('../config/mongo.js').url();
+
+// Connect to mongo DB and set events
+const db = mongojs( mongoUrl );
+
+db.on('error', (err) => {
+	console.log('database error', err);
+});
+db.on('connect', () => {
+	console.log('database connected');
+});
+db.on('close', () => {
+	console.log('database disconnected');
+});
+
+module.exports = db;
