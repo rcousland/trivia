229283cd17dbf6c4853ea61aae1cd55f0eb30749
Rory <rcousland@gmail.com>
2018-01-14 14:39:02 +0000

    games collection commands now work

--- controllers/schema.js
+++ controllers/schema.js
@@ -5,22 +5,25 @@ const mongoUrl = require('../config/mongo.js').url();
 const db = mongojs( mongoUrl );
 require('./mongo.js')(db);
 
-var userCollection = db.collection('users'),
-	qaCollection = db.collection('questionsAndAnswers'),
-	gamesCollection = db.collection('games');
+var gamesCollection = db.collection('games');
+// userCollection = db.collection('users'),
+// qaCollection = db.collection('questionsAndAnswers');
 
 
-module.exports = {
-	gameIdFind: (gameId) => {
-		gamesCollection.findOne( gameId, (err, docs) => {
-			if(err)throw new Error(err);
-            
-			else{
-				return (docs);
-			}
-		});
-	},
+module.exports.gamesFindOne = (query, callback) => {
+	gamesCollection.findOne( query, (err, doc) => {
+		if(err) { return callback(err,null);}
+		else{ return callback(null,doc);}
+	});
 };
+
+module.exports.gamesInsert = (newDoc, callback) => {
+	gamesCollection.insert( newDoc, (err, doc) => {
+		if(err) { return callback(err,null);}
+		else{ return callback(null,doc);}
+	});
+};
+
 
 
 
