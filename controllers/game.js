40fc03d72101d0b576b8c79073a92d25be6ccf5d
Rory <rcousland@gmail.com>
2018-01-14 14:40:15 +0000

    removed game.js - restructed functions to smaller files in controllers/games dir

--- controllers/game.js
+++ controllers/game.js
@@ -1,31 +1,4 @@
-const schema = require('./schema.js')();
 
-module.exports.newGame = (callback) => {
-	var gameId;
-
-	gameIdGenerate();
-	function gameIdGenerate(){
-		gameId = Math.floor(Math.random()*90000) + 10000; //generates random 5-digit number
-		schema.gameIdFind( gameId, (err,data) => {
-			if (err){
-				return callback(err,null);
-			}
-			else{ //test
-				if(gameId != data){
-					callback(null,gameId);
-				}
-				else {
-					gameIdGenerate();
-				}
-			}
-		});
-
-	}
-	// start new game....
-	// create "gameID"
-	// send gameID and first question
-
-};
 // module.exports.userAnswer = () =>{
 
 //         // post answer to question
