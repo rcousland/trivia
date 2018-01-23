d9d291f6b929445a0c3d3f8c0091aefb58239206
Rory Cousland <r.cousland@mtl.local>
2018-01-23 20:22:33 +0000

    updated questions and answers controller locations

--- controllers/game/index.js
+++ controllers/game/index.js
@@ -1,3 +1,4 @@
 module.exports.newGame = require('./newGame.js');
-module.exports.userAnswer = require('./userAnswer.js');
+module.exports.getQuestion = require('./getQuestion.js');
+module.exports.answer = require('./answer.js');
 module.exports.highScore = require('./highScore.js');
