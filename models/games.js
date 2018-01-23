79d002235ba2a1a4e402f9d2b2f0025f4a9795c9
Rory Cousland <r.cousland@mtl.local>
2018-01-23 20:24:27 +0000

    updated model error message. added findAndModify mongo method

--- models/games.js
+++ models/games.js
@@ -1,18 +1,23 @@
 const db = require('../controllers/mongo.js');
 const collection = db.collection('games');
 
+const mongoErr = {'Error':'Mongo Error'};
+
 module.exports.findOne = (query, callback) => {
 	collection.findOne( query, (err, doc) => {
-		if(err) { return callback('mongo error',null);}
+		if(err) { return callback(mongoErr,null);}
 		else{ return callback(null,doc);}
 	});
 };
 module.exports.insert = (newDoc, callback) => {
 	collection.insert( newDoc, (err, doc) => {
-		if(err) { return callback('mongo error',null);}
+		if(err) { return callback(mongoErr,null);}
 		else{ return callback(null,doc);}
 	});
 };
-module.exports.update = (query, newDoc, callback) => {
-	// update game with users questions and answers
+module.exports.findAndModify = (query, callback) => {
+	collection.findAndModify( query, (err, doc) => {
+		if(err) { return callback(mongoErr,null);}
+		else { return callback(null,doc);}
+	});
 };
