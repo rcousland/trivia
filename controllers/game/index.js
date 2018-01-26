000bbf0201f8a7165148f385f5af99efe95666f0
Rory Cousland <r.cousland@mtl.local>
2018-01-26 20:03:44 +0000

    add enterscore changed  question

--- controllers/game/index.js
+++ controllers/game/index.js
@@ -1,4 +1,5 @@
 module.exports.newGame = require('./newGame.js');
-module.exports.getQuestion = require('./getQuestion.js');
+module.exports.question = require('./question.js');
 module.exports.answer = require('./answer.js');
+module.exports.enterScore = require('./enterScore.js');
 module.exports.highScore = require('./highScore.js');
