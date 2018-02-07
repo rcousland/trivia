362e25de2ac3737bebdc7f5c1d3628990a76603a
Rory Cousland <r.cousland@mtl.local>
2018-02-07 22:24:21 +0000

    /newgame /highscore /question /answer = async/await

--- routes/api/game.js
+++ routes/api/game.js
@@ -4,55 +4,47 @@ const jsonParser = require('body-parser').json();
 
 var game = require('../../controllers/game/');
 
-router.get('/newgame', (req, res) => {
-	game.newGame( (err,data) => {
-		if(err){ 
-			res.status( 500 ).send(err);
-		}
-		else{
-			res.json(data);
-		}
-	});
-
-	// start new game....
-	// create "gameID"
-	// send gameID
-});
-router.get('/highScore', async (req, res) => {
+router.get('/newgame', async (req, res) => { // generate new gameId. pass to client
 	try {
-		result = await game.highScore()
+		const result = await game.newGame()
 		res.json( result );
 	} catch(e) {
-		res.status( 500 ).send(e)
+		const errResponse = {'err': e }
+		res.status( 500 ).json( errResponse )
 	}
 });
 
-router.post('/question', jsonParser, (req, res) => {
-	var gameId = req.body.gameId;
-	game.question( gameId, (err,data) => {
-		if(err){ 
-			res.status( 500 ).send(err);
-		}
-		else{
-			res.json(data);
-		}
-	});
-	// get next question
+router.get('/highScore', async (req, res) => { // get top 10 results
+	try {
+		const result = await game.highScore()
+		res.json( result );
+	} catch(e) {
+		const errResponse = {'err':e}
+		res.status( 500 ).json(errResponse)
+	}
 });
 
-router.post('/answer', jsonParser, (req, res) => {
-	var gameId = req.body.gameId;
-	var userAnswer = req.body.userAnswer;
-	game.answer( gameId, userAnswer , (err,data) => {
-		if(err){ 
-			res.status( 500 ).send(err);
-		}
-		else{
-			res.json(data);
-		}
-	});
-	// post answer to question
-	// get response.
+router.post('/question', jsonParser, async (req, res) => { // get next question
+	const gameId = req.body.gameId;
+	try {
+		const result = await game.question( gameId )
+		res.json( result );
+	} catch(e) {
+		const errResponse = {'err':e}
+		res.status( 500 ).json( errResponse )
+	}
+});
+
+router.post('/answer', jsonParser, async (req, res) => { // post answer to question. get response.
+	const gameId = req.body.gameId;
+	const userAnswer = req.body.userAnswer;
+	try {
+		const result = await game.answer( gameId , userAnswer)
+		res.json( result );
+	} catch(e) {
+		const errResponse = {'err':e}
+		res.status( 500 ).json( errResponse )
+	}
 });
 
 router.post('/enterScore', jsonParser, (req, res) => {
