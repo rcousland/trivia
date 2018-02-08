ade400e241e0fa3aa56db566b7f6f5f567abc040
Rory Cousland <r.cousland@mtl.local>
2018-02-08 23:00:01 +0000

    fixed error handler

--- controllers/game/highScore.js
+++ controllers/game/highScore.js
@@ -3,10 +3,9 @@ const model = require('../../models/');
 module.exports = () => new Promise( async (resolve, reject) => { // show 10 highest scores.
 	try {
 		const result = await model.highScore.findTop10Scores();
-		const empty = {'Trivia':'No Highscore data entered'}
-			if(result.length > 0 ){ resolve( result ); } 
-			else{ resolve(empty); }
+			if(result.length == 0 ){ resolve( {'game.highscore':'No Highscore data entered'} ) } 
+			else{ resolve( result ) }
 	} catch(e) {
-		reject(err)
+		reject(e)
 	}
 });
