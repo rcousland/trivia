5a33e9a26e6c0061b79919b66496d10b7725ec42
Rory Cousland <r.cousland@mtl.local>
2018-02-09 12:37:26 +0000

    cleaned up route middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const e = require('./config/express.js');
+const gameRoutes = require('./routes/api/game.js')
+const catchAllRoute = require('./routes/catchAll.js')
 
 // Load routes
 app.use(express.static('public')); // static files in /public dir
-app.use('/api/game', require('./routes/api/game.js')); // game routes
-app.use( (req,res) => { require('./routes/catchAll.js')(req,res) }); //catch all
+app.use('/api/game', gameRoutes); // game routes
+app.use(catchAllRoute); //catch all
 
 // Start express
 app.listen(e.port, e.host, () => {
--- routes/catchAll.js
+++ routes/catchAll.js
@@ -1,22 +1,20 @@
 const path = require('path');
 
-module.exports = (req,res) =>{
+module.exports = (req,res, next) =>{
     res.status(404)
 
     // respond with html page
     if (req.accepts('html')) {
         res.sendFile( path.join(__dirname + '/../public/404/index.html') )
-        return;
     }
 
     // respond with json
     else if (req.accepts('json')) {
-        res.send( {error: 'Not found'} );
-        return;
+        res.send( {'error': 'Path not found'} );
     }
 
     // default to plain-text. send()
     else {
-        res.type('txt').send('Not found');
+        res.type('txt').send('Path not found');
     }
 }
