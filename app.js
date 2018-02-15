3b4d76b8ae2becc9b6f9a98f22bb8b33d1d5c506
Rory Cousland <r.cousland@mtl.local>
2018-02-15 23:29:00 +0000

    added mongo handler

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express();
 const e = require('./config/express.js');
+const getCollections = require('./controllers/mongo.js')
 const gameRoutes = require('./routes/api/game.js')
 const catchAllRoute = require('./routes/catchAll.js')
 
-// Catch error codes to console.log
-app.use( morgan(':method :url :status :res[content-length] - :response-time ms') );
+load()
+async function load(){
+    try{
+        // Connect to mongoDB and return collection methods
+        var collections = await getCollections()
 
-// Load Routes
-app.use(express.static('public')); // static files in /public dir
-app.use('/api/game', gameRoutes); // game routes
-app.use(catchAllRoute); //catch all
+        // Catch error codes to console.log
+        app.use( morgan(':method :url :status :res[content-length] - :response-time ms') );
 
-// Start Express
-app.listen(e.port, e.host, () => {
-	console.log(e.host + ' listening on port: ' + e.port);
-});
+        // Load Routes
+        app.use(express.static('public')); // static files in /public dir
+        app.use('/api/game', gameRoutes(collections)); // game routes
+        app.use(catchAllRoute); //catch all
+
+        // Start Express
+        app.listen(e.port, e.host, () => {
+            console.log(e.host + ' listening on port: ' + e.port);
+        });
+    }catch(e){
+        console.log(e)
+    }
+}
