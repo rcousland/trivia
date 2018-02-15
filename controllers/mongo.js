4fd8ef083cef3fe589e3dfefcde8e0d0fa927fcc
Rory Cousland <r.cousland@mtl.local>
2018-02-15 23:32:24 +0000

    replaced mongojs with mongo native

--- controllers/mongo.js
+++ controllers/mongo.js
@@ -1,17 +1,14 @@
-const mongojs = require('mongojs');
-const m = require('../config/mongo.js');
+const mongodb = require('mongodb');
+var url = "mongodb://localhost:27017/";
+var dbName = 'trivia'
 
-// Connect to mongo DB and set events
-const db = mongojs( m.url() , [] , m.options );
-
-db.on('error', (err) => {
-	console.log('database error', err);
-});
-db.on('connect', () => {
-	console.log('database connected');
-});
-db.on('close', () => {
-	console.log('database disconnected');
-});
-
-module.exports = db;
+module.exports = async function(){
+    const db = await mongodb.MongoClient.connect(url)
+    const dbo = db.db(dbName)       
+    return {
+        games: dbo.collection("games"),
+        questionsAndAnswers: dbo.collection("questionsAndAnswers"),
+        highScore: dbo.collection("highScore"),
+        db: db
+    }
+}
