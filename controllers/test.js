173d148edecbba19e827e15556ee5b8e0a1810bd
Rory <rcousland@gmail.com>
2018-02-15 00:23:27 +0000

    promise test

--- controllers/test.js
+++ controllers/test.js
@@ -1,14 +1,27 @@
-const mongoUpdateAnswerCmd = require('./mongoUpdateAnswerCmd.js');
+const p1 = Promise.resolve(3);
+const p2 = 42;
 
-ggg()
+const p3 = new Promise( (resolve, reject) => {
+    //setTimeout( ()=> console.log('gg'), 3000 )
+    setTimeout(resolve, 4000, 'foo');
+});
+const p4 = new Promise( (resolve, reject) => {
+    //setTimeout( ()=> console.log('gg'), 3000 )
+    setTimeout(resolve, 4000, 'foo');
+});
 
-async function ggg(){
-const gameId = 43421
-const questionId = 1
-const userAnswer = 'o4'
-const realAnswer = 'o4'
-
-const mongoCmd = new mongoUpdateAnswerCmd(gameId, questionId, userAnswer, realAnswer);
-const json = await JSON.stringify(mongoCmd)
-console.log(json)
+ff()
+async function ff(){
+    console.log( await p1 )
+    console.log( await p2 )
+    console.log( await p3 )
+    console.log(  await p4 )
+}
+async function gg(){
+    console.log( await Promise.all([p1, p2, p3]) )
+    //const all = await Promise.all([p1, p2, p3])
+    //console.log(all)
+}
+function hh(){
+    Promise.all([p1, p2, p3]).then( v => console.log(v) );
 }
