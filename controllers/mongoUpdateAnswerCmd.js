e8df630c7592e8510e5df7db9f1dcb0d489e3da8
Rory Cousland <r.cousland@mtl.local>
2018-02-15 23:31:18 +0000

    refactored to work with mongo native driver

--- controllers/mongoUpdateAnswerCmd.js
+++ controllers/mongoUpdateAnswerCmd.js
@@ -1,6 +1,6 @@
 module.exports = function (gameId, questionId, userAnswer, realAnswer) {
 	this.query = {'gameId': gameId };
-	this.update = {
+	var update = {
 		$push: {
 			userAnswers: {
 				'questionId' : questionId,
@@ -11,12 +11,26 @@ module.exports = function (gameId, questionId, userAnswer, realAnswer) {
 			nextQuestion: 1 
 		}
 	};
-	this.new = true;
 	if (userAnswer == realAnswer){
-		this.update.$push.userAnswers.correctAnswer = true;
-		this.update.$inc.score = 1;
+		update.$push.userAnswers.correctAnswer = true;
+		update.$inc.score = 1;
 	}
 	else{
-		this.update.$push.userAnswers.correctAnswer = false;	
+		update.$push.userAnswers.correctAnswer = false;	
 	}
+	this.update = update
+	this.returnNewDoc = {new:true}
 };
+
+
+// collection.findAndModify({a:1}, [['a', 1]], {$set:{b1:1}}, {new:true}, function(err, doc) {
+
+// 	db.collection("collection_name").findAndModify(
+// 		{ _id: _id },     // query
+// 		[],               // represents a sort order if multiple matches
+// 		{ $set: data },   // update statement
+// 		{ new: true },    // options - new to return the modified document
+// 		function(err,doc) {
+	
+// 		}
+// 	);
