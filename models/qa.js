cedf8c1573ef3f90c81aee61c0f916108ba46878
Rory Cousland <r.cousland@mtl.local>
2018-02-15 23:33:07 +0000

    remove unused models

--- models/qa.js
+++
@@ -1,9 +0,0 @@
-const db = require('../controllers/mongo.js');
-var collection = db.collection('questionsAndAnswers');
-
-module.exports.findOne = (query) => new Promise( (resolve, reject) => {
-	collection.findOne( query, (err, doc) => {
-		if(err){ reject(err); }
-		else{ resolve(doc); } 
-	});
-});
