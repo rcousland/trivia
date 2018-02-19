6f40bc2f27434eaab0e086f234a5e5bd85dfd032
Rory <rcousland@gmail.com>
2018-02-19 09:05:26 +0000

    mod config fiel for mongo driver

--- config/mongo.js
+++ config/mongo.js
@@ -1,13 +1,12 @@
-module.exports = {
-	host: process.env.MONGO_HOST || 'localhost',
-	port: process.env.MONGO_PORT || 27017,
-	db: process.env.MONGO_DB || 'trivia',
-	options: {
-		autoReconnect: true,
-		reconnectTries: 5,
-		reconnectInterval: 1000
-	},
-	url: function() {
-		return this.host + ':' + this.port + '/' + this.db;
-	}
+module.exports = new function(){
+	var data = {
+		host : process.env.MONGO_HOST || 'localhost',
+		port : process.env.MONGO_PORT || 27017,
+		dbName: process.env.MONGO_DBNAME || 'trivia',
+		options : {
+			appname : process.env.APP_NAME || 'trivia app'
+		}
+	};
+	data.url = 'mongodb://' + data.host + ':' + data.port + '/' + data.dbName;
+	return data;
 };
--- controllers/mongo.js
+++ controllers/mongo.js
@@ -1,14 +1,13 @@
 const mongodb = require('mongodb');
-var url = "mongodb://localhost:27017/";
-var dbName = 'trivia'
+const m = require('../config/mongo.js');
 
 module.exports = async function(){
-    const db = await mongodb.MongoClient.connect(url)
-    const dbo = db.db(dbName)       
-    return {
-        games: dbo.collection("games"),
-        questionsAndAnswers: dbo.collection("questionsAndAnswers"),
-        highScore: dbo.collection("highScore"),
-        db: db
-    }
-}
+	const db = await mongodb.MongoClient.connect(m.url, m.options);
+	const dbo = db.db(m.dbName);       
+	return {
+		games: dbo.collection('games'),
+		questionsAndAnswers: dbo.collection('questionsAndAnswers'),
+		highScore: dbo.collection('highScore'),
+		db: db
+	};
+};
