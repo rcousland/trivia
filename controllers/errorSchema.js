d8f5d4af2d20707f65b25b5220ada4583bf619a1
Rory Cousland <r.cousland@mtl.local>
2018-03-06 21:59:42 +0000

    removed error control

--- controllers/errorSchema.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (errType, message, sourceFile, line) {
-	if(errType = 'err') this.err = errType;
-	else if(errType = 'missing') this.missing = errType;
-	this.message = message;
-	this.sourceFile = sourceFile;
-	this.line = line - 1;
-};
--- controllers/sourceFile.js
+++
@@ -1,3 +0,0 @@
-module.exports = (sourceFileName) => {
-	return sourceFileName.replace(global.rootDir,'');
-};
