d0ef1c389f788f51de2e5084071f2f70c9c8596b
Rory Cousland <r.cousland@mtl.local>
2018-03-12 19:24:51 +0000

    update docker cmd. auto restart

--- scripts/bash/app-run.sh
+++ scripts/bash/app-run.sh
@@ -1,10 +1,11 @@
 #run mongodb docker
-sudo docker run -d -p 27017:27017 -v /data/db:/data/db mongo
+sudo docker run -d --restart unless-stopped -p 27017:27017 -v /data/db:/data/db mongo
 
 # donload git source. then setup docker images and start. run as appuser
 # clone git
 cd /data/app
 git clone https://github.com/rcousland/trivia.git
+cd trivia
 
 # install packages
 npm install
